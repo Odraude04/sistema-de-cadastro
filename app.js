3524368b650c86e4ece5637afd4233bffec31466
Odraude04 <eduardo_queiros1@hotmail.com>
2022-08-31 15:42:58 -0300

    adicionando uns consoles.log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando sistema de gerenciamento de jogos.")
 console.log("Aplicação está se conectando ao banco de dados.")
+//mais um console.log de teste:
+console.log("O professorfes esse comit sem você ver na aula.")
+console.log("Enviando para o GitHub.")
